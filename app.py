cbc6821d83140bde75dcb2c28cb83aacf47236fe
kapil sharma <sharmakapil49996@gmail.com>
2025-06-19 15:56:02 +0530

    Second commit

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+from sklearn.datasets import load_iris  
+from sklearn.decomposition import PCA
+# Load the iris dataset
